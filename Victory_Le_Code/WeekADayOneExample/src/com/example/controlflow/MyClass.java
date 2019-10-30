27a4c4e69a39157c394693c215752ae52f4ba0b6
VictoryDev <victoryle1993@gmail.com>
2019-10-30 15:39:36 -0400

    files were in staging for a minute

---
+++ Victory_Le_Code/WeekADayOneExample/src/com/example/controlflow/MyClass.java
@@ -0,0 +1,29 @@
+package com.example.controlflow;
+
+public class MyClass {
+	/*
+	 * Control flow statements?
+	 * They break up the flow of execution by decision making and loops, enabling the program to conditionally execute blocks of code
+	 * 
+	 * if -> if true run block of code, "else if" is used to write mutually exclusive blocks of code, "else" will execute if no condition was met
+	 * 
+	 * for -> typically is used when you know for how long you want to loop, has a start, an end and an increment.
+	 * 
+	 * switch -> useful when you are checking a variable against a lot of conditions/values
+	 * 
+	 * while -> typically used when you do not know how long you want to loop but have condition that needs to be satisfied. while conditions are true, code are running the whole time, breaks out when condition is no longer met.
+	 * 
+	 *  do-while -> execute at least one time, then acts as a while. 
+	 *  
+	 *  "continue" - go to the end of the loop and start at the next iteration; (can be used in switch and loops).
+	 *  
+	 *  "break" - goes to end of loop and breaks out of the loop and switch
+	 */
+	
+	public static void main (String[] args) {
+		/*
+		 * if statement
+		 * a random number is generated, if the number is greater than 0,
+		 * 
+		 */
+}
