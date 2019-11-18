584fb3bc41981b0da5f61b6f2a49583a2e88e9d6
MichaelJAButterfield <41044503+MichaelJAButterfield@users.noreply.github.com>
2019-11-18 17:00:20 -0500

    Add files via upload

---
+++ MichaelButterfieldProjectZero/src/bankaccount/Administrator.java
@@ -0,0 +1,12 @@
+package bankaccount;
+
+public class Administrator extends Customer  {
+
+	Administrator(String username, String password) {
+		super(username, password);
+	}
+
+	
+	
+	
+}
---
+++ MichaelButterfieldProjectZero/src/bankaccount/Employee.java
@@ -0,0 +1,27 @@
+package bankaccount;
+
+public class Employee  {
+	
+	protected int empId;
+	protected String empName;
+	
+	
+	 protected Employee(){
+		this.empId = 1234;
+		this.empName = "Malcom Warner";
+		}
+		
+	public void info() {
+		System.out.println("Id: " +empId+" Name: "+empName);
+	}
+	
+	
+	
+	
+	
+	
+	}
+	
+
+
+
