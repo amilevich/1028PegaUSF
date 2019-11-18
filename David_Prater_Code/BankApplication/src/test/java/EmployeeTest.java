5e0020bb5cca2af96f85288868d319ac16a81dfc
David Prater <davidrussprater@gmail.com>
2019-11-18 14:37:59 -0500

    Added test case for random account number range

--- David_Prater_Code/BankApplication/src/test/java/EmployeeTest.java
+++ David_Prater_Code/BankApplication/src/test/java/EmployeeTest.java
@@ -1,9 +1,17 @@
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.Test;
 
 public class EmployeeTest {
 
+
+
+	Random r = new Random();
+	int randomAccountNumber = r.nextInt((100000 - 1) + 1) + 1;
+	int outOfRangeAccountNumber = 10000000;
+	
 
 	/*
 	 * No Testable Methods.
@@ -11,4 +19,15 @@ public class EmployeeTest {
 	 * 
 	 */
 
+	@Test
+	public void generateRandomAccountNumberTrue() {
+		assertTrue("account is in range: " + randomAccountNumber, randomAccountNumber > 0 && randomAccountNumber < 100000);
+		System.out.println("Random Account Number Range True");
+	}
+	
+	@Test
+	public void generateRandomAccountNumberFalse() {
+		assertFalse("account is out of range: " + outOfRangeAccountNumber, outOfRangeAccountNumber > 0 && outOfRangeAccountNumber < 100000);
+		System.out.println("Random Account Number is out of Range True");
+	}	
 }
