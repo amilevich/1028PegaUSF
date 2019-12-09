00576911274edb22f552fd70a03fd3ee594c2872
jochisoto <jochisoto@gmail.com>
2019-12-09 01:17:33 -0500

    Add files via the Shadow Realm

--- Felix_Soto_code/Project1/src/main/webapp/js/adminhome.js
+++ Felix_Soto_code/Project1/src/main/webapp/js/adminhome.js
@@ -168,3 +168,34 @@ function denyExpense(e) {
     xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhttp.send("value=" + e.target.value);
 }
+
+
+
+function filterTable(event) {
+    var filter = event.target.value.toUpperCase();
+    var rows = document.querySelector("#reimbtable").rows;
+    
+    for (var i = 0; i < rows.length; i++) {
+        var firstCol = rows[i].cells[0].textContent.toUpperCase();
+        var secondCol = rows[i].cells[1].textContent.toUpperCase();
+        var thirdCol = rows[i].cells[2].textContent.toUpperCase();
+        var fourthCol = rows[i].cells[3].textContent.toUpperCase();
+        var fifthCol = rows[i].cells[4].textContent.toUpperCase();
+        var sixthCol = rows[i].cells[5].textContent.toUpperCase();
+        var seventhCol = rows[i].cells[6].textContent.toUpperCase();
+        var EighthCol = rows[i].cells[7].textContent.toUpperCase();
+        var ninthCol = rows[i].cells[8].textContent.toUpperCase();
+        var tenthCol = rows[i].cells[9].textContent.toUpperCase();
+
+        if (firstCol.indexOf(filter) > -1 || secondCol.indexOf(filter) > -1 || thirdCol.indexOf(filter) > -1|| fourthCol.indexOf(filter) > -1
+        		|| fifthCol.indexOf(filter) > -1|| sixthCol.indexOf(filter) > -1 
+        		|| seventhCol.indexOf(filter) > -1|| EighthCol.indexOf(filter) > -1 
+        		|| ninthCol.indexOf(filter) > -1 || tenthCol.indexOf(filter) > -1) {
+            rows[i].style.display = "";
+        } else {
+            rows[i].style.display = "none";
+        }      
+    }
+}
+
+document.querySelector('#myInput').addEventListener('keyup', filterTable, false);
